81714cc8da88d687832fe55a4013dd8f9a93dd17
Ammar Husain <mrahusain@gmail.com>
2017-09-15 09:49:03 -0700

    working version of rostopic echo; gotta figure out how include generate pb2, set pythonpath

---
+++ python/ros_protobuf_bridge/msg/Bar.py
@@ -0,0 +1,78 @@
+#! /usr/bin/python
+
+import Bar_pb2
+import sys
+import genpy
+import struct
+python3 = True if sys.hexversion > 0x03000000 else False
+
+class Bar(genpy.Message):
+    _md5sum = "003ab2255988927d7e6b13ccbf717699"
+    _type = "ros_protobuf_bridge/Bar"
+    _has_header = False
+    _full_text = """ros_protobuf_bridge::Bar is not a traditional ROS msg and is not
+  defined in the ROS message IDL, though would look something like uint8[]
+  serialized_data
+  """
+    __slots__ = ['bar_data']
+    _slot_types = ['custom']
+
+    def __init__(self, *args, **kwds):
+        """
+    Constructor.
+    """
+        self.bar_data = Bar_pb2.Bar()
+
+    def _get_types(self):
+        """
+    internal API method
+    """
+        return self._slot_types
+
+    def serialize(self, buff):
+        """
+    serialize message into buffer
+    :param buff: buffer, ``StringIO``
+    """
+        try:
+            buff = self.bar_data.SerializeToString()
+        except struct.error as se:
+            self._check_types(
+                struct.error("%s: '%s' when writing '%s'" % (type(se), str(se), str(locals().get(
+                    '_x', self)))))
+        except TypeError as te:
+            self._check_types(
+                ValueError("%s: '%s' when writing '%s'" % (type(te), str(te), str(locals().get(
+                    '_x', self)))))
+
+    def deserialize(self, str):
+        """
+    unpack serialized message in str into this message instance
+    :param str: byte array of serialized message, ``str``
+    """
+        print 'Called deserialize'
+        try:
+            self.bar_data = Bar_pb2.Bar()
+            self.bar_data.ParseFromString(str)
+            print self.bar_data
+            return self
+        except struct.error as e:
+            raise genpy.DeserializationError(e)  # Most likely buffer underfill
+
+    def serialize_numpy(self, buff, numpy):
+        """
+    serialize message with numpy array types into buffer
+    :param buff: buffer, ``StringIO``
+    :param numpy: numpy python module
+    """
+        self.serialize(buff)
+
+    def deserialize_numpy(self, str, numpy):
+        """
+    unpack serialized message in str into this message instance using numpy for array types
+    :param str: byte array of serialized message, ``str``
+    :param numpy: numpy python module
+    """
+        print 'Called deserialize numpy'
+        self.deserialize(str)
+        return self
