d51707e449641de2d591df3f736111ca20f55bc0
Ammar Husain <mrahusain@gmail.com>
2017-09-15 17:26:30 -0700

    added compounded proto; cleaned up cmake

--- src/pub.cpp
+++ src/pub.cpp
@@ -1,5 +1,4 @@
 #include "ros/ros.h"
-#include "ros_protobuf_bridge/Foo.h"
 #include "Bar.ros.h"
 
 #include <string>
@@ -10,37 +9,24 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "talker");
 
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<ros_protobuf_bridge::Bar>("chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise<ros_protobuf_bridge::FooBar>("chatter", 1000);
 
   ros::Rate loop_rate(10);
 
-  ros_protobuf_bridge::Bar prot, d_prot;
+  ros_protobuf_bridge::FooBar prot;
 
   int count = 0;
-  std::string ser;
   while (ros::ok())
   {
-
-    ros_protobuf_bridge::Foo obj;
-
-    obj.a = count;
-    ROS_INFO("%d", obj.a);
-    // chatter_pub.publish(obj);
-
     // prot.set_page_number(count);
     for (int i = 0; i < count; i++) {
-      prot.add_some_strings("foo_string");
+      prot.mutable_b()->add_some_strings("foo_string");
     }
-    prot.SerializeToString(&ser);
+    chatter_pub.publish(prot);
 
-    d_prot.ParseFromString(ser);
-    // ROS_INFO("Bar: %d", d_prot.page_number());
-    chatter_pub.publish(d_prot);
-
-    prot.clear_some_strings();
-
+    ROS_INFO("str sz: %d", prot.mutable_b()->some_strings_size());
+    prot.mutable_b()->clear_some_strings();
     ros::spinOnce();
-
     loop_rate.sleep();
     ++count;
     count = count % 15;
--- src/sub.cpp
+++ src/sub.cpp
@@ -1,26 +1,21 @@
-#include "ros/ros.h"
-#include "ros_protobuf_bridge/Foo.h"
 #include "Bar.ros.h"
+#include "ros/ros.h"
 
-void chatterCallback(const ros_protobuf_bridge::Foo& msg)
-{
-  ROS_INFO("I heard: [%d]", msg.a);
-}
-
-void chatterCallbackBar(const ros_protobuf_bridge::Bar& msg)
-{
-  // ROS_INFO("I heard: [%d] with %d string", msg.page_number(), msg.some_strings_size());
-    ROS_INFO("I heard: [%d] with %d string", 1, msg.some_strings_size());
+void chatterCallbackBar(const ros_protobuf_bridge::Bar &msg) {
+  ROS_INFO("I heard: [%d] with %d string", 1, msg.some_strings_size());
   for (int i = 0; i < msg.some_strings_size(); ++i) {
     ROS_INFO("%s", msg.some_strings(i).c_str());
   }
 }
 
-int main(int argc, char **argv)
-{
+void chatterCallbackFooBar(const ros_protobuf_bridge::FooBar &msg) {
+  return chatterCallbackBar(msg.b());
+}
+
+int main(int argc, char **argv) {
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallbackBar);
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallbackFooBar);
   ros::spin();
 
   return 0;
